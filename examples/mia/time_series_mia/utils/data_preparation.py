56caf3b6d0fec6e3f73e6fd07d58b7db02d27d12
Nicolas Johansson <89980249+ZyzzGit@users.noreply.github.com>
2025-02-11 21:24:23 +0100

    Fix dataset indexing

--- examples/mia/time_series_mia/utils/data_preparation.py
+++ examples/mia/time_series_mia/utils/data_preparation.py
@@ -1,16 +1,29 @@
 import os
 import pickle
+import joblib
 import numpy as np
 
 from sklearn.preprocessing import MinMaxScaler
-from numpy import float32
+from torch import float32
 from torch import tensor
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, Dataset, Subset
 
-class ECGDataset(TensorDataset):
-    def __init__(self, x:tensor, y:tensor, indices:np.ndarray):
-        super().__init__(x, y)
-        self.indices = indices  # the indices correspond to the raw timesteps (before creating sequences)
+class ECGDataset(Dataset):
+    def __init__(self, x:tensor, y:tensor):
+        self.x = x
+        self.y = y
+
+        self.lookback = x.size(1)
+        self.horizon = y.size(1)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+    
+    def subset(self, indices):
+        return ECGDataset(self.x[indices], self.y[indices])
 
 def to_sequences(data, lookback, horizon):
     x, y = [], []
@@ -20,46 +33,47 @@ def to_sequences(data, lookback, horizon):
     for t in range(num_samples):
         x.append(data[t:t + lookback, :])
         y.append(data[t + lookback:t + lookback + horizon, :])
-    return np.array(x, dtype=float32), np.array(y, dtype=float32)
+    return tensor(x, dtype=float32), tensor(y, dtype=float32)
 
+def preprocess_ECG_dataset(path, lookback, horizon):
+    """Get and preprocess the dataset."""
 
-def get_ECG_dataloaders(lookback, horizon, train_fraction):
-    """Preprocesses data and returns data loaders."""
+    if os.path.exists(os.path.join(path, "ECG_E00001.pkl")):
+        with open(os.path.join(path, "ECG_E00001.pkl"), "rb") as f:
+            dataset = joblib.load(f)
+    else:
+        file = os.path.join(path, "ECG_E00001.npy")
+        data = np.load(file, allow_pickle=True)
 
-    path = os.path.join(os.getcwd(), "data")
-    file = os.path.join(path, "ECG_E00001.npy")
-    data = np.load(file, allow_pickle=True)
+        # Scale all variables to range [0, 1]
+        scaler = MinMaxScaler()
+        data_scaled = scaler.fit_transform(data)
 
-    timesteps = len(data)
-    split_idx = int(timesteps * train_fraction)
-    data_train, data_test = data[:split_idx, :], data[split_idx:, :]
+        # Create sequence dataset
+        x, y = to_sequences(data_scaled, lookback, horizon)
+        dataset = ECGDataset(x, y)
 
-    # Scale all variables to range [0, 1]
-    scaler = MinMaxScaler()
-    data_train = scaler.fit_transform(data_train)
-    data_test = scaler.transform(data_test)
+        # Save dataset
+        with open(f"{path}/ECG_E00001.pkl", "wb") as file:
+            pickle.dump(dataset, file)
+            print(f"Save data to {path}.pkl") 
 
-    # Save preprocessed dataset as pickle
-    processed_data = np.concatenate([data_train, data_test], axis=0)
-    with open(f"{path}/ECG_E00001.pkl", "wb") as file:
-        pickle.dump(processed_data, file)
-        print(f"Save data to {path}.pkl")  
+    return dataset
 
-    x_train, y_train = to_sequences(data_train, lookback, horizon)
-    x_test, y_test = to_sequences(data_test, lookback, horizon)
+def get_ECG_dataloaders(dataset: ECGDataset, train_fraction):
 
-    train_dataset = ECGDataset(
-        tensor(x_train), 
-        tensor(y_train),
-        indices = np.arange(0, split_idx)
-    )
-    test_dataset = ECGDataset(
-        tensor(x_test), 
-        tensor(y_test),
-        indices = np.arange(split_idx, timesteps)
-    )
+    seq_len = dataset.lookback + dataset.horizon
+    non_overlapping_len = len(dataset) - seq_len - 1    # the number of samples when splitting dataset in two parts such that no time points overlap
 
-    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+    train_end_idx = int(non_overlapping_len  * train_fraction)
+    test_begin_idx = train_end_idx + seq_len
+    train_indices = np.arange(0, train_end_idx)
+    test_indices = np.arange(test_begin_idx, len(dataset))
+
+    train_subset = Subset(dataset, train_indices) 
+    test_subset = Subset(dataset, test_indices)
+
+    train_loader = DataLoader(train_subset, batch_size=128, shuffle=True)
+    test_loader = DataLoader(test_subset, batch_size=128, shuffle=False)
 
     return train_loader, test_loader
